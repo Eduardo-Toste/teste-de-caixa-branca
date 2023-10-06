ef3b454d308b2892eb6bc9c395c35ae78d6fa714
Eduardo Toste <duciochetti@gmail.com>
2023-10-06 01:24:50 -0300

    Atualizando ETAPA2

--- src/main/test.java
+++ src/main/test.java
@@ -9,28 +9,28 @@ import java.sql.Statement;
 
 /*2*/public static void main(String[] args) {
 /*3*/	public Connection conectarBD() {
-			Connection conn = null;
-/*4*/		try {
+/*4*/		Connection conn = null;
+			try {
 				Class.forName("com.mysql.Driver.Manager").newInstance();
 				String url = "jdbc:mysql://157.0.0.1/test?user=lopes&password=123";
-				conn = DriverManager.getConnection(url);
-/*5*/		} catch (Exception e) { }
+/*5*/			conn = DriverManager.getConnection(url);
+			} catch (Exception e) { }
 			return conn; }
 /*6*/	public String nome="";
 /*7*/	public boolean result = false;
 /*8*/	public boolean verificarUsuario(String login, String senha) {
-			String sql = "";
-			public Connection conectarBD();
+/*9*/			String sql = ""; 
+/*10*/			public Connection conectarBD();
 			//INSTRUÇÃO SQL
-			sql += "select nome from usuario ";
-			sql += "where login = " + "'" + login + "'";
-			sql += " and senha = " + "'" + senha + "';";
-/*9*/		try {
-				Statement st = conn.createStatement();
-				ResultSet rs = st.executeQuery(sql);
-/*10*/			if(rs.next()) {
-					result = true;
-					nome = rs.getString("nome");}
-/*11*/			} catch (Exception e) { }
+/*11*/		sql += "select nome from usuario ";
+/*12*/		sql += "where login = " + "'" + login + "'";
+/*13*/		sql += " and senha = " + "'" + senha + "';";
+			try {
+/*14*/			Statement st = conn.createStatement();
+/*15*/			ResultSet rs = st.executeQuery(sql);
+/*16*/			if(rs.next()) {
+/*17*/				result = true;
+/*18*/				nome = rs.getString("nome");}
+				} catch (Exception e) { }
 			return result;
 			} // fim da class
