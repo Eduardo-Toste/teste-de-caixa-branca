b52511f52a97a64cc0b2b08cbd76ae913575e8aa
Eduardo Toste <duciochetti@gmail.com>
2023-10-19 20:22:39 -0300

    Codigo Documentado

--- src/main/test.java
+++ src/main/test.java
@@ -5,32 +5,72 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-	 public class test {
+/**
+ * A classe Test é responsável por conectar a um banco de dados MySQL e verificar um usuário.
+ */
+public class Test {
 
-	 public static void main(String[] args) {
-		 public Connection conectarBD() {
-			 Connection conn = null;
-			try {
-				Class.forName("com.mysql.Driver.Manager").newInstance();
-				String url = "jdbc:mysql://157.0.0.1/test?user=lopes&password=123";
-				conn = DriverManager.getConnection(url);
-			} catch (Exception e) { }
-			return conn; }
-		public String nome="";
-		public boolean result = false;
-		public boolean verificarUsuario(String login, String senha) {
-				String sql = ""; 
-				public Connection conectarBD();
-			//INSTRUÇÃO SQL
-			sql += "select nome from usuario ";
-			sql += "where login = " + "'" + login + "'";
-			sql += " and senha = " + "'" + senha + "';";
-			try {
-				Statement st = conn.createStatement();
-				ResultSet rs = st.executeQuery(sql);
-				if(rs.next()) {
-					result = true;
-					nome = rs.getString("nome");}
-				} catch (Exception e) { }
-			return result;
-			} // fim da class
+    /**
+     * Método para estabelecer uma conexão com o banco de dados MySQL.
+     * @return A conexão com o banco de dados ou null se ocorrer um erro na conexão.
+     */
+    public Connection conectarBD() {
+        Connection conn = null;
+        try {
+            // Carrega o driver JDBC do MySQL
+            Class.forName("com.mysql.Driver.Manager").newInstance();
+            // Configuração da URL de conexão com o banco de dados
+            String url = "jdbc:mysql://157.0.0.1/test?user=lopes&password=123";
+            conn = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    /**
+     * Nome do usuário recuperado após a verificação.
+     */
+    public String nome = "";
+
+    /**
+     * Resultado da verificação do usuário.
+     */
+    public boolean result = false;
+
+    /**
+     * Verifica a existência de um usuário no banco de dados.
+     * @param login O nome de usuário a ser verificado.
+     * @param senha A senha do usuário a ser verificada.
+     * @return true se o usuário existe no banco de dados, false caso contrário.
+     */
+    public boolean verificarUsuario(String login, String senha) {
+        Connection conn = conectarBD();
+        if (conn == null) {
+            return false;
+        }
+
+        String sql = "SELECT nome FROM usuario " +
+                     "WHERE login = '" + login + "' " +
+                     "AND senha = '" + senha + "';";
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()) {
+                result = true;
+                nome = rs.getString("nome");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Fecha a conexão com o banco de dados
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
